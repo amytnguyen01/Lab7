eddb5643660d07d773dc8022143fdd1b503500cc
amytnguyen01 <59705411+amytnguyen01@users.noreply.github.com>
2021-05-17 23:29:33 -0700

    Update sw.js
    service worker intro couldn't push through vscode so manually copied pasted into github.com

--- sw.js
+++ sw.js
@@ -1,5 +1,49 @@
 // sw.js - Service Worker
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+}
+
+var CACHE_NAME = 'my-site-cache-v1'
+var urlsToCache = ['https://cse110lab6.herokuapp.com/entries'];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+  self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+  });
+
 // You will need 3 event listeners:
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
